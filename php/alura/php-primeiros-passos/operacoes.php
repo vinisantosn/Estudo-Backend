85401f32d988822a1431bb4f048939b8e2c3053b
Vinícius Santos <vinisantoosk@gmail.com>
2022-06-08 12:12:03 -0300

    feat: aulas e exercicios

---
+++ php/alura/php-primeiros-passos/operacoes.php
@@ -0,0 +1,18 @@
+<?php
+
+$idade = 22;
+$minhaIdadeDaqui10Anos = $idade + 10;
+$minhaIdadeHa5Anos = $idade - 5;
+
+echo $minhaIdadeDaqui10Anos;
+echo $minhaIdadeHa5Anos;
+
+$soma = 9+9;
+$subtracao = 8-8;
+$multiplicacao = 7*7;
+$divisao = 6/6;
+$potencia = 2**5;
+$restoDaDivisao = 10 % 3;
+
+
+echo $soma,$subtracao,$multiplicacao,$divisao;
